8528974e398243cd1c76c84d75c34d2629b81ebc
petru <ale19@LG-Gram-15Z980.homenet.telecomitalia.it>
2020-05-03 16:29:49 +0200

    remove morestrings

--- main.go
+++ main.go
@@ -4,13 +4,11 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/Petrus97/bif-parser/morestrings"
 	"github.com/Petrus97/bif-parser/parser"
 )
 
 func main() {
 	fmt.Println("Hello")
-	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 	start := time.Now()
 	myNet := parser.ReadBIF("data/survey.bif")
 	fmt.Println(time.Now().Sub(start))
