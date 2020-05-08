43eef58fc138312b97969824c842b2749a3afbc8
ale19 <ale.petru97@gmail.com>
2020-05-08 17:08:15 +0200

    add stuff

--- bayesnet/junctiontree.go
+++ bayesnet/junctiontree.go
@@ -9,6 +9,7 @@ package bayesnet
 
 // Clique aka Vertex of Junction Tree
 type Clique struct {
+	Name       string
 	Variables  []*Node // Nodes that form the cluster
 	Neighbours []*Clique
 	Separators []*Separator
@@ -17,8 +18,10 @@ type Clique struct {
 
 // Separator -> intersection of nodes between two Cliques
 type Separator struct {
-	Variables []*Node // Node intersection between two cluster
-	Table     [][]float64
+	Variables   []*Node // Node intersection between two cluster
+	Table       [][]float64
+	LeftClique  *Clique
+	RightClique *Clique
 }
 
 // JunctionTree structure
@@ -37,4 +40,32 @@ func (jt *JunctionTree) addSeparators(separators ...*Separator) {
 	for _, sep := range separators {
 		jt.Separators = append(jt.Separators, sep)
 	}
+}
+
+func (c *Clique) AddVariables(nodes ...*Node) {
+	for _, node := range nodes {
+		c.Variables = append(c.Variables, node)
+	}
+}
+
+// InitIcyRoadJT initialize junctiontree relative to icy_roads.bif
+func InitIcyRoadJT(bn *BN) (jt *JunctionTree) {
+	iw := Clique{
+		Name:       "IW",
+		Variables:  make([]*Node, 0),
+		Neighbours: make([]*Clique, 0),
+		Separators: make([]*Separator, 0),
+		Table:      make([][]float64, 0),
+	}
+	iw.AddVariables(bn.GetNode("Icy"), bn.GetNode("Watson"))
+	ih := Clique{
+		Name:       "IH",
+		Variables:  make([]*Node, 0),
+		Neighbours: make([]*Clique, 0),
+		Separators: make([]*Separator, 0),
+		Table:      make([][]float64, 0),
+	}
+	ih.AddVariables(bn.GetNode("Icy"), bn.GetNode("Holmes"))
+	var junc JunctionTree
+	return &junc
 }
